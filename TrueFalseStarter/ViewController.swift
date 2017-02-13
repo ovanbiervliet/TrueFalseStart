23ede1ab77ced3dfaf14149c36378d67c2ec92dd
ovan <ovan+gitkraken@coreland.be>
2017-02-13 21:09:18 +0100

    ~ App is universal now ~ Answer button tags now contain 1 for correct answer, 0 for wrong answer + Added high score + Adds emojiLabel to show emoji on final score display + Trivia is based on shuffled array + Each question is only asked once ~ Last round can have less than questionsPerRound questions * Score calculation based on questionsAsked instead of questionsPerRound * Color buttons fixed

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -12,14 +12,28 @@ import AudioToolbox
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    
     let questionsPerRound = 4
     var questionsAsked = 0
     var correctQuestions = 0
     var indexOfSelectedQuestion: Int = 0
     let maxAnswers = 4
+    var highScore = 0
     let buttonNormalColor = UIColor(red: 0x49 / 255, green: 0x50 / 255, blue: 0x57 / 255, alpha: 1.0)
     let buttonChosenColor = UIColor(red: 0xC9 / 255, green: 0x2A / 255, blue: 0x2A / 255, alpha: 1.0)
     let buttonCorrectColor = UIColor(red: 0x2F / 255, green: 0x9E / 255, blue: 0x44 / 255, alpha: 1.0)
+    enum emoji: String {
+        case score0 = "🙁"
+        case score1 = "😕"
+        case score2 = "😐"
+        case score3 = "🙂"
+        case score4 = "😀"
+    }
     
     
     var gameSound: SystemSoundID = 0
@@ -27,27 +41,28 @@ class ViewController: UIViewController {
     // Setup a trivia provider
     // & prepare first trivia
     let triviaProvider = TriviaProvider()
+    var shuffledTrivia: [Trivia] = []
     var currentTrivia = Trivia(question: "", choices: [], answer: 0)
     
-    @IBOutlet weak var questionField: UILabel!
-    @IBOutlet var answerButtons: [UIButton]!
-    @IBOutlet weak var playAgainButton: UIButton!
-    @IBOutlet weak var scoreLabel: UILabel!
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Get an array of shuffled Trivia questions
+        shuffledTrivia = triviaProvider.shuffledTrivia()
+        
         // Prepare game
         loadGameStartSound()
+        playGameStartSound()
         
-        // Setup button colors when disabled
+        // Setup button title color when disabled
         for button in answerButtons {
             button.setTitleColor(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .disabled)
         }
 
-        // Start game
-        playGameStartSound()
+        // Hide the score emoji
+        emojiLabel.isHidden = true
         
+        // Start game
         displayQuestion()
     }
 
@@ -56,100 +71,112 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /// Hides the answer buttons when game level is finished
     func hideAnswerButtons() {
         for button in answerButtons {
           button.isHidden = true
         }
+        emojiLabel.isHidden = false
     }
     
+    /// Locks answer buttons after one was tapped,
+    /// to avoid accidental extraneous taps.
     func lockAnswerButtons(_ locked: Bool) {
         for button in answerButtons {
             button.isEnabled = !locked
         }
     }
     
-    func showCorrectAnswer(answer: Int, triviaAnswer: Int) {
-        // check every button and change styling to show
-        // the correct answer
-        for button in answerButtons {
-            // show choosen answer
-            button.tag == answer
-                ? (button.backgroundColor = buttonChosenColor)
-                : (button.backgroundColor = buttonNormalColor)
-            
-            // show correct answer
-            if (button.tag == triviaAnswer) {
-                button.backgroundColor = buttonCorrectColor
-            }
-            
-        }
-        // prevent additional clicks
-        lockAnswerButtons(true)
-    }
-    
+    /// Fetches and displays a question
     func displayQuestion() {
         // Get a new trivia
-        currentTrivia = triviaProvider.randomTrivia()
+        //currentTrivia = triviaProvider.randomTrivia()
         
-        // Setup the question and buttons
-        questionField.text = currentTrivia.question
-        let numAnswers = currentTrivia.choices.count
-        for index in 0..<maxAnswers {
-            // reference current button
-            let button = answerButtons[index]
+        // Try to get a new Trivia
+        if let currentTrivia = triviaProvider.nextTrivia(from: &shuffledTrivia) {
+            // Setup the question
+            questionField.text = currentTrivia.question
             
-            // if this is an active button, set title
-            if index < numAnswers {
-                button.setTitle(currentTrivia.choices[index], for: UIControlState.normal)
-                button.backgroundColor = buttonNormalColor
+            // Determine number of answers
+            let numAnswers = currentTrivia.choices.count
+            
+            // Configure the buttons
+            for index in 0..<maxAnswers {
+                
+                // reference current button
+                let button = answerButtons[index]
+                
+                // if this is an active button, set title
+                if index < numAnswers {
+                    button.setTitle(currentTrivia.choices[index], for: UIControlState.normal)
+                    button.backgroundColor = buttonNormalColor
+                    
+                    // Set tag to 1 if button contains correct answer,
+                    // otherwise set it to zero
+                    button.tag = (index + 1 == currentTrivia.answer) ? 1 : 0
+                }
+                
+                // set visibility of button
+                button.isHidden = index >= numAnswers
             }
-
-            // set visibility of button
-            button.isHidden = index >= numAnswers
+            playAgainButton.isHidden = true
+            lockAnswerButtons(false)
+        } else {
+            // there are no more Trivia questions
+            displayFinalScore()
         }
-
-        playAgainButton.isHidden = true
-        lockAnswerButtons(false)
+        
     }
     
+    /// Updates the in-game score display
     func updateScore() {
-        if (scoreLabel.isHidden) { scoreLabel.isHidden = false }
-        scoreLabel.text = "Score: \(correctQuestions)"
+        //if (scoreLabel.isHidden) { scoreLabel.isHidden = false }
+        scoreLabel.text = "Score: \(correctQuestions)\nHigh Score: \(highScore)"
     }
     
-    func displayFinalScore() {
-        // Hide the answer buttons
-        hideAnswerButtons()
-        
-        // Hide the small score label
-        scoreLabel.isHidden = true
-        
-        // Display play again button
-        playAgainButton.isHidden = false
-        
-        questionField.text = "You got \(correctQuestions) out of \(questionsPerRound) correct!"
-        
-    }
-    
+    /// Gets called when one of the answer buttons is tapped
     @IBAction func checkAnswer(_ sender: UIButton) {
         // Increment the questions asked counter
         questionsAsked += 1
         
-        // the chosen answer is the title of the button that is pushed
+        // the chosen answer is correct when its tag == 1
         let answer = sender.tag
 
-        showCorrectAnswer(answer: answer, triviaAnswer: currentTrivia.answer)
-        
-        if (answer == currentTrivia.answer) {
+        if (answer == 1) {
             correctQuestions += 1
+            if (correctQuestions > highScore) { highScore = correctQuestions }
             questionField.text = "Correct!"
         } else {
             questionField.text = "Sorry, wrong answer!"
         }
         
+        showCorrectAnswer(answer: sender, triviaAnswer: currentTrivia.answer)
+
         updateScore()
         
         loadNextRoundWithDelay(seconds: 2)
+    }
+    
+    /// Shows color-coded buttons based on chosen and correct answers
+    func showCorrectAnswer(answer: UIButton, triviaAnswer: Int) {
+
+        // check every button and change styling to show
+        // the correct answer
+        for button in answerButtons {
+            
+            // set correct answer green, others neutral
+            button.tag == 1
+                ? (button.backgroundColor = buttonCorrectColor)
+                : (button.backgroundColor = buttonNormalColor)
+            
+            // if answer was wrong, make it red
+            if (answer.tag != 1) {
+                answer.backgroundColor = buttonChosenColor
+            }
+            
+        }
+        // prevent additional clicks
+        lockAnswerButtons(true)
     }
     
     func nextRound() {
@@ -163,10 +190,45 @@ class ViewController: UIViewController {
     }
     
     @IBAction func playAgain() {
+        emojiLabel.isHidden = true
         questionsAsked = 0
         correctQuestions = 0
         updateScore()
         nextRound()
+    }
+    
+    /// Displays the score when game is finished
+    func displayFinalScore() {
+        // Hide the answer buttons
+        hideAnswerButtons()
+        
+        // Hide the small score label
+        //scoreLabel.isHidden = true
+        
+        // Show correct emoji
+        let scorePercent = Int(100 * correctQuestions / questionsAsked)
+        switch scorePercent {
+        case 0 ..< 25:
+            emojiLabel.text = emoji.score0.rawValue
+        case 25 ..< 50:
+            emojiLabel.text = emoji.score1.rawValue
+        case 50 ..< 75:
+            emojiLabel.text = emoji.score2.rawValue
+        case 75 ..< 100:
+            emojiLabel.text = emoji.score3.rawValue
+        case 100:
+            emojiLabel.text = emoji.score4.rawValue
+        default:
+            emojiLabel.text = "🦑"
+        }
+        emojiLabel.isHidden = false
+        
+        
+        // Display play again button
+        playAgainButton.isHidden = false
+        
+        questionField.text = "You got \(correctQuestions) out of \(questionsAsked) correct!"
+        
     }
     
 
--- TrueFalseStarter/triviaProvider.swift
+++ TrueFalseStarter/triviaProvider.swift
@@ -8,39 +8,56 @@
 
 import GameKit
 
-class Trivia {
+let allTrivia: [Trivia] = [
+    Trivia(question: "Only female koalas can whistle", choices: ["True", "False"], answer: 2),
+    Trivia(question: "Blue whales are technically whales", choices: ["True", "False"], answer: 1),
+    Trivia(question: "Camels are cannibalistic", choices: ["True", "False"], answer: 2),
+    Trivia(question: "All ducks are birds", choices: ["True", "False"], answer: 1),
+    Trivia(question: "This was the only US President to serve more than two consecutive terms.", choices: ["George Washington", "Franklin D. Roosevelt", "Woodrow Wilson",  "Andrew Jackson"], answer: 2),
+    Trivia(question: "Which of the following countries has the most residents?", choices: ["Nigeria", "Russia", "Iran", "Vietnam"], answer: 1),
+    Trivia(question: "In what year was the United Nations founded?", choices: ["1918", "1919", "1945", "1954"], answer: 3),
+    Trivia(question: "The Titanic departed from the United Kingdom, where was it supposed to arrive?", choices: ["Paris", "Washington D.C.", "New York City", "Boston"], answer: 3),
+    Trivia(question: "Which nation produces the most oil?", choices: ["Iran", "Iraq", "Brazil", "Canada"], answer: 4),
+    Trivia(question: "Which country has most recently won consecutive World Cups in Soccer?", choices: ["Italy", "Brazil", "Argentina", "Spain"], answer: 2),
+    Trivia(question: "Which of the following rivers is longest?", choices: ["Yangtze", "Mississippi", "Congo", "Mekong"], answer: 2),
+    Trivia(question: "Which city is the oldest?", choices: ["Mexico City", "Cape Town", "San Juan", "Sydney"], answer: 1),
+    Trivia(question: "Which country was the first to allow women to vote in national elections?", choices: ["Poland", "United States", "Sweden", "Senegal"], answer: 1),
+    Trivia(question: "Which of these countries won the most medals in the 2012 Summer Games?", choices: ["France", "Germany", "Japan", "Great Britain"], answer: 4),
+]
+
+struct Trivia {
     let question: String
     let choices: [String]
     let answer: Int
     
-    init(question: String, choices: [String], answer: Int) {
-        self.question = question
-        self.choices = choices
-        self.answer = answer
-    }
+//    init(question: String, choices: [String], answer: Int) {
+//        self.question = question
+//        self.choices = choices
+//        self.answer = answer
+//    }
 }
 
-class TriviaProvider {
-    let allTrivia: [Trivia] = [
-        Trivia(question: "Only female koalas can whistle", choices: ["True", "False"], answer: 2),
-        Trivia(question: "Blue whales are technically whales", choices: ["True", "False"], answer: 1),
-        Trivia(question: "Camels are cannibalistic", choices: ["True", "False"], answer: 2),
-        Trivia(question: "All ducks are birds", choices: ["True", "False"], answer: 1),
-        Trivia(question: "This was the only US President to serve more than two consecutive terms.", choices: ["George Washington", "Franklin D. Roosevelt", "Woodrow Wilson",  "Andrew Jackson"], answer: 2),
-        Trivia(question: "Which of the following countries has the most residents?", choices: ["Nigeria", "Russia", "Iran", "Vietnam"], answer: 1),
-        Trivia(question: "In what year was the United Nations founded?", choices: ["1918", "1919", "1945", "1954"], answer: 3),
-        Trivia(question: "The Titanic departed from the United Kingdom, where was it supposed to arrive?", choices: ["Paris", "Washington D.C.", "New York City", "Boston"], answer: 3),
-        Trivia(question: "Which nation produces the most oil?", choices: ["Iran", "Iraq", "Brazil", "Canada"], answer: 4),
-        Trivia(question: "Which country has most recently won consecutive World Cups in Soccer?", choices: ["Italy", "Brazil", "Argentina", "Spain"], answer: 2),
-        Trivia(question: "Which of the following rivers is longest?", choices: ["Yangtze", "Mississippi", "Congo", "Mekong"], answer: 2),
-        Trivia(question: "Which city is the oldest?", choices: ["Mexico City", "Cape Town", "San Juan", "Sydney"], answer: 1),
-        Trivia(question: "Which country was the first to allow women to vote in national elections?", choices: ["Poland", "United States", "Sweden", "Senegal"], answer: 1),
-        Trivia(question: "Which of these countries won the most medals in the 2012 Summer Games?", choices: ["France", "Germany", "Japan", "Great Britian"], answer: 4),
-    ]
-    
-    /// Return a random trivia question
+struct TriviaProvider {
+    /// Returns a random Trivia question
     func randomTrivia() -> Trivia {
         let randomNumber = GKRandomSource.sharedRandom().nextInt(upperBound: allTrivia.count)
         return allTrivia[randomNumber]
+    }
+
+    /// Returns the first availabie Trivia question from an array
+    /// and removes it from that array
+    func nextTrivia(from remainingTrivia: inout [Trivia]) -> Trivia? {
+        // If there are no more Trivia, return
+        if (remainingTrivia.count == 0) { return nil }
+
+        // Get the first one and remove it from the supplied array
+        let nextTrivia: Trivia = remainingTrivia.removeFirst()
+        
+        return nextTrivia
+    }
+    
+    /// Returns a shuffled array of Trivia questions
+    func shuffledTrivia() -> [Trivia] {
+        return GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allTrivia) as! [Trivia]
     }
 }
